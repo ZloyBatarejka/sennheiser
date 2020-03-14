01d5fd7f70c6bbd0a4ffa8801f9eb007b0f12f1a
Yakovlev Ruslanz <57760502+ZloyBatarejka@users.noreply.github.com>
2020-03-14 12:50:08 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,32 @@
+$(".popular-slick").slick({
+  dots: true,
+  infinite: true,
+  centerMode: true,
+  slidesToShow: 3,
+  slidesToScroll: 3,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+  ]
+});
